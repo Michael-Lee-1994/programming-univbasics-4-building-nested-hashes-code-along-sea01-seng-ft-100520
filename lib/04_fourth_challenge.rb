ee78751df68b91f39c25db59a2fb4e145e581b0c
Michael Lee <michael.l33.1994@gmail.com>
2020-09-22 10:01:55 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -6,8 +6,8 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
-        {name: "Benovlio",
-        
+        {name: "Benovlio", age: "17", attitude: "worried"},
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    }, 
    :capulet => {
